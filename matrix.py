be16426a6c7203c1574ac3a0118647a49c841c02
Tony <antoniu2002@gmail.com>
2022-04-13 11:05:59 +0300

    Display matrix function added.

--- matrix.py
+++ matrix.py
@@ -3,4 +3,13 @@ def generate_matrix():
     for i in range(0, 9):
         matrix.append([])
         for j in range(0, 9):
-            matrix[i].append(0)
+            matrix[i].append('\u2588') # \u2588 means █ in unicode
+def display_matrix():
+    for i in range(0, 9):
+        print(i + 1, end = '')
+        for j in range(0, 9): print(matrix[i][j], end = '') # Parsing each line
+        print()
+    print(' ', end = '')
+    for i in range(0, 9): print(i + 1, end = '')
+# generate_matrix()
+# display_matrix()
